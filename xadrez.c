1c7c91ca6cb00a77176e498eb2a6ccd348fd9ed0
emillykkjjk <gangchan.tere@gmail.com>
2025-10-27 00:55:43 +0000

    fiz mais coisas

--- xadrez.c
+++ xadrez.c
@@ -1,8 +1,43 @@
 #include <stdio.h>
   int main(){
-    printf("Desafio Xadrez\n");
-    printf("novo commit\n");
+    printf("Desafio Xadrez");
+    printf("novo commit");
 
     return 0;
-    ]
-}
+    
+  }
+
+  int main(){
+
+    char estado1;
+    char codigo1[4];
+    char nomeCidade1[50];
+    int populacao1;
+    float area1;
+    float pib1;
+    int pontosTuristicos1;
+
+
+    printf("digite seu estado:\n ");
+    scanf(" %c", &estado1); 
+
+    printf("digite o nome da cidade: \n");
+    scanf(" %s", &nomeCidade1);  
+
+    printf("digite a população: \n");
+    scanf(" %d", &populacao1);  
+
+    printf("digite a área: \n");
+    scanf(" %e", &area1);  
+
+    printf("digite o pib: \n");
+    scanf(" %e", &pib1);  
+
+    printf("digite os pontos turísticos: \n");..
+      scanf(" %d", &pontosTuristicos1);  
+
+
+return 0; 
+
+
+  }
